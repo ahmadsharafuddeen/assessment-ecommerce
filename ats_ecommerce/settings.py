0d7dfedb09c6488a217a9f83727fc02da4b9abd5
Ahmad Sharafudeen <asharafudeen@afexnigeria.com>
2022-08-24 15:13:01 +0100

    added all hosts allowed

--- ats_ecommerce/settings.py
+++ ats_ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-7p3dd4hz1zpv2q2xr!@pkajed6mxb#!^3yq-43n=x)lx9a)-58
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
