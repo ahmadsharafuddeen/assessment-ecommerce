09e5cb5553bb30e0fcfe1bfc39968bec839f1c21
Ahmad Sharafudeen <asharafudeen@afexnigeria.com>
2022-09-01 09:43:32 +0100

    set debug to false

--- ats_ecommerce/settings.py
+++ ats_ecommerce/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7p3dd4hz1zpv2q2xr!@pkajed6mxb#!^3yq-43n=x)lx9a)-58'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
